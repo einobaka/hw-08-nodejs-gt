9b6307727d8b1ac784c02ef14c80aedf877735db
einobaka <61556425+einobaka@users.noreply.github.com>
2020-06-02 21:35:03 -0700

    added inquier code base

--- index.js
+++ index.js
@@ -7,23 +7,77 @@ inquirer.prompt([
         name: "username",
         message: "What is your GitHub username?",
     },
-
+    {
+        type: "input",
+        name: "projectitle",
+        message: "What is your Project Title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project.",
+    },
+    // {
+    //     type: "input",
+    //     name: "contents",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "installation",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "usage",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "license",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "contributing",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "tests",
+    //     message: "",
+    // },
+    // {
+    //     type: "input",
+    //     name: "questions",
+    //     message: "",
+    // },
 
 ])
+    .then(answers => {
+        console.log(answers.username);
+        console.log(answers.projectitle);
+        console.log(answers.description);
+        console.log("end of logs");
+
+    })
+    .catch(error => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        }
+    });
 
 
 
-const questions = [
+// const questions = [
 
+// ];
 
+// function writeToFile(fileName, data) {
+// }
 
-];
+// function init() {
 
-function writeToFile(fileName, data) {
-}
+// }
 
-function init() {
-
-}
-
-init();
+// init();
